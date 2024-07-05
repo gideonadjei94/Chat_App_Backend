4b29b17dc5820e13e9f0a61bddfa579a7fad3625
gideonadjei <gideonadjei94@gmail.com>
2024-07-05 13:27:33 -0700

    Server

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express, { urlencoded } from "express";
+import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import { dbConnection } from "./utils/Db.js";
