fc1e07eede9f71712522ce1d5ad6e65812f1293f
gideonadjei <gideonadjei94@gmail.com>
2024-07-07 21:49:26 -0700

    Implemented Sockets

--- utils/Socket.js
+++ utils/Socket.js
@@ -1,20 +1,34 @@
 import { Server } from "socket.io";
 
 let io;
-
+let users = {};
 export function initialize() {
   io = new Server();
 
   io.on("connection", (socket) => {
-    console.log("New client connected");
-
-    socket.on("join", ({ userId }) => {
-      socket.join(userId);
-      console.log(`User with ID: ${userId} joined the room`);
-    });
+    console.log("New client connected :D");
 
     socket.on("disconnect", () => {
-      console.log("Client disconnected");
+      console.log("Client disconnected :(");
+      for (const userId in users) {
+        if (users[userId] === socket.id) {
+          delete users[userId];
+          break;
+        }
+      }
+    });
+
+    socket.on("register", (userId) => {
+      users[userId] = socket.id;
+      console.log(`User ${userId} registered with socket id ${socket.id}`);
+    });
+
+    socket.on("chat-message", (data) => {
+      //console.log(data);
+      const receiverSocketId = users[data.receiverId];
+      if (receiverSocketId) {
+        io.to(receiverSocketId).emit("getMessage", data);
+      }
     });
 
     //add a message
